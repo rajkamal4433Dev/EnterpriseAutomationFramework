83f1bc44f44cf296404212dfd2292e46450a9a6b
rajkamal4433Dev <rajkamal4433@gmail.com>
2026-06-01 11:46:02 +0530

    commiting changes to devlopbranch

---
+++ src/test/java/com/framework/tests/developbranchTest.java
@@ -0,0 +1,12 @@
+package com.framework.tests;
+
+import org.testng.annotations.Test;
+
+public class developbranchTest {
+	
+	@Test(priority = 1, description = "Test to check develop branch")
+	public void developBranchTest() {
+		System.out.println("This is the develop branch test");
+	}
+
+}
